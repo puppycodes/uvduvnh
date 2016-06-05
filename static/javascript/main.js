f83fc20af9fdff62b7d9ec5a6971446290ded0c2
Rye Skelton <ryeskelton@gmail.com>
2016-06-05 03:02:03 -0700

    change random to click and push all items from galleries to front page

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,21 +1,21 @@
-$( document ).ready(function () {
+
     var current = 0,
 
-        $imgs = jQuery('#cycle .art');
+    $imgs = jQuery('#cycle .art');
     imgAmount = $imgs.length;
 
     $($imgs.css('position', 'absolute').hide().get(0)).show();
 
 
-    window.setInterval(swapImages, 1000);
-
-    function swapImages() {
-        
+     $('.art').on( "click", function() {
+          
         var $currentImg = $('.art:visible');
         
         var $nextImg = $('.art:hidden').eq(Math.floor(Math.random() * $('.art:hidden').length));
             speed = 500;
         $currentImg.fadeOut(speed);
         $nextImg.fadeIn(speed);
-    }
-});
+  
+
+    });
+
